5b12f86bfdc3440d323df88dc82880cb148cd2a3
chrisraffertyse@gmail.com <chrisraffertyse@gmail.com>
2023-11-18 19:38:55 +0000

    Added parent page model with loginpage

---
+++ Models/Pages/DefaultPage.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+
+namespace TestAutomationFramework.Models.Pages
+{
+    internal class DefaultPage
+    {
+        protected IWebDriver Driver;
+        protected virtual String BaseUrl { get; } = "https://www.saucedemo.com/";
+        protected virtual string Path { get; } = string.Empty;
+
+        internal DefaultPage(IWebDriver driver, int waitTimeInSeconds = 1)
+        {
+            Driver = driver;
+            Driver.Manage().Timeouts().ImplicitWait = System.TimeSpan.FromSeconds(waitTimeInSeconds);
+        }
+
+        internal void Open()
+        {
+            Driver.Navigate().GoToUrl(BaseUrl + Path);
+        }
+    }
+}
---
+++ Models/Pages/LoginPage.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutomationFramework.Models.Pages
+{
+    internal class LoginPage : DefaultPage
+    {
+        private readonly string usernameFieldSelector = "#user-name";
+        private readonly string passwordFieldSelector = "#password";
+        private readonly string loginButtonSelector = "#login-button";
+        private readonly string errorMessageSelector = "div.error-message-container > h3";
+        internal LoginPage(IWebDriver driver, int waitTimeInSeconds = 1) : base(driver, waitTimeInSeconds)
+        {
+        }
+
+        public string ErrorMessageContent()
+        {
+            try 
+            {
+                return Driver.FindElement(By.CssSelector(errorMessageSelector)).Text;
+            } 
+            catch (NoSuchElementException)
+            {
+                return "Error message not found";
+            }
+        }
+
+        public void AttemptLogin(string username, string password)
+        {
+            Driver.FindElement(By.CssSelector(usernameFieldSelector)).SendKeys(username);
+            Driver.FindElement(By.Id(passwordFieldSelector)).SendKeys(password);
+            Driver.FindElement(By.Id(loginButtonSelector)).Click();
+        }
+    }
+}
