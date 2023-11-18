f31c29056270c07540f950cf95a1b16d349b759d
chrisraffertyse@gmail.com <chrisraffertyse@gmail.com>
2023-11-18 23:13:10 +0000

    Implemented generic features and step definitions files

---
+++ Features/LoginPage.feature
@@ -0,0 +1,5 @@
+﻿Feature: Login Validation
+
+Scenario: Verify visibility of the login page
+    Given I am on the login page
+    Then I should see that I am on the login page
---
+++ StepDefinitions/LoginPage.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+using TestAutomationFramework.Models.Pages;
+
+namespace YourNamespace.StepDefinitions
+{
+    [Binding]
+    public class LoginValidationSteps
+    {
+        private readonly IWebDriver driver;
+        private readonly LoginPage loginPage;
+
+        public LoginValidationSteps()
+        {
+            driver = new ChromeDriver();
+            loginPage = new LoginPage(driver);
+        }
+
+        [Given("I am on the login page")]
+        public void GivenIAmOnTheLoginPage()
+        {
+            loginPage.Open();
+        }
+
+        [Then("I should see that I am on the login page")]
+        public void ThenIShouldSeeThatIAmOnTheLoginPage()
+        {
+            bool isLoginPageDisplayed = loginPage.IsDisplayed();
+
+            Assert.IsTrue(isLoginPageDisplayed, "Expected to be on the login page, but it's not.");
+        }
+    }
+}
