cd136d66463aed43d2beced3e21b2f854bdcd57a
chrisraffertyse@gmail.com <chrisraffertyse@gmail.com>
2023-11-19 10:09:18 +0000

    Added productpage model

---
+++ Models/Pages/ProductPage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutomationFramework.Models.Pages
+{
+    internal class ProductPage : DefaultPage
+    {
+        private readonly string spanTitleSelector = ".title";
+        internal ProductPage(IWebDriver driver, int waitTimeInSeconds = 1) : base(driver, waitTimeInSeconds)
+        {
+        }
+
+        internal override bool IsDisplayed()
+        {
+            try
+            {
+                return Driver.FindElement(By.CssSelector(spanTitleSelector)).Displayed;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+    }
+}
